3ea65b12a34d5d6114ee96677a53603d8dd4f816
Yunfah Chow <yunfah@yunfahs-mbp.clients.internal.mah.se>
2019-05-09 14:43:28 +0200

    Shooting and hud

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 30;
+    public int currentHealth;
+    public AudioClip deathClip;
+    public float sinkSpeed = 2.5f;
+
+    AudioSource enemyAudio;
+    //ParticleSystem hitParticales;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake()
+    {
+        enemyAudio = GetComponent<AudioSource>();
+        //hitParticales = GetComponent<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        currentHealth = startingHealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage(int amount, Vector3 hitPoints)
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        enemyAudio.Play();
+        currentHealth -= amount;
+       // hitParticales.transform.position = hitPoints;
+      //  hitParticales.Play();
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+
+        StartSinking();
+
+    }
+
+    public void StartSinking()
+    {
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+
+        Destroy(gameObject, 2f);
+
+    }
+}
---
+++ Assets/Scripts/playerShooting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShooting : MonoBehaviour
+{
+    public int damagePerShot = 10;
+    public float timeBewteenBullets = 0.15f;
+    public float range = 100f;
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectDisplayTime = 0.2f;
+
+    void Awake()
+    {
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBewteenBullets)
+        {
+            Shoot();
+        }
+
+        if (timer >= timeBewteenBullets * effectDisplayTime)
+        {
+            DisableEffects();
+
+        }
+    }
+
+    void DisableEffects()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    void Shoot()
+    {
+        timer = 0f;
+        gunAudio.Play();
+
+        gunLight.enabled = true;
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if(Physics.Raycast (shootRay, out shootHit, range))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+            if(enemyHealth != null)
+            {
+               enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+            }
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+
+    }
+}
