5edc2ab981e824499af10cdd89bb8fce899da90d
Yunfah Chow <yunfah@yunfahs-mbp.clients.internal.mah.se>
2019-05-09 14:35:51 +0200

    shooting and new hud

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    //Health
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+    public bool damaged;
+
     public float speed;
     public Text start, end, ammoText, healthText;
     private Rigidbody rb;
@@ -12,6 +19,8 @@ public class Controller : MonoBehaviour
     private int health;
     private bool gameEnded;
     int startValue = 100;
+
+    AudioSource dmgTaken; 
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +32,8 @@ public class Controller : MonoBehaviour
         health = 9;
         SetHealthText();
         gameEnded = false;
+        dmgTaken = GetComponent<AudioSource>();
     }
-
 
 
     // Update is called once per frame
@@ -83,7 +92,7 @@ public class Controller : MonoBehaviour
 
     void SetHealthText()
     {
-        healthText.text = "Health left: " + health.ToString();
+        healthText.text = "Health: ";
     }
 
     void OnTriggerEnter(Collider other)
@@ -93,21 +102,31 @@ public class Controller : MonoBehaviour
             end.text = "YOU HAVE REACHED THE GOAL!\n    PRESS 'K' TO RESTART";
             gameEnded = true;
         }
+
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             ammo += 5;
             SetAmmoText();
         }
+
         if (other.gameObject.CompareTag("Enemy"))
         {
             health -= 1;
-            SetHealthText();
+            healthSlider.value = health;
+
+            dmgTaken.Play();
+
             if (health == 0)
             {
+                damageImage.color = flashColor;
                 end.text = "  YOU HAVE LOST\n    PRESS 'K' TO RESTART";
                 gameEnded = true;
             }
+
+
         }
     }
+
+
 }
